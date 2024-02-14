032581fc20a266dc39967f7bd87fc481a127380b
msalemor <44649358+msalemor@users.noreply.github.com>
2024-02-14 13:02:28 -0500

    Updated the yahoo finance period

--- notebooks/common.py
+++ notebooks/common.py
@@ -9,7 +9,7 @@ email_URI: str = ''
 
 def get_stock_price(symbol: str) -> float:
     stock = yf.Ticker(symbol)
-    price = stock.history(period="1d")['Close'].iloc[-1]
+    price = stock.history(period="1mo")['Close'].iloc[-1]
     return price
 
 
