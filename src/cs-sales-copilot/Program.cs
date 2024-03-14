039d732389caf42b76cda454fab06e0e103af265
msalemor <44649358+msalemor@users.noreply.github.com>
2024-03-14 13:51:18 -0400

    Added c# sales copilot

---
+++ src/cs-sales-copilot/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure;
+using Azure.AI.OpenAI.Assistants;
+using agent;
+
+// Load the settings from the .env file
+AgentSettings settings = new();
+
+// Create the C# Assistants client
+AssistantsClientOptions options = new(AssistantsClientOptions.ServiceVersion.V2024_02_15_Preview);
+AssistantsClient client = new(new Uri(settings.APIEndpoint), new AzureKeyCredential(settings.APIKey), options);
+
+// Create the Sales agent and registration
+AssistantAgent salesAgent = await SalesAgent.SalesAgent.GetAgent(settings, client);
+AgentRegistration salesAgentRegistration = new(salesAgent,
+  "SalesIntent",
+  "You are an assistant that can answer questions related to customers, sellers, orders and inventory.");
+
+// Create the trading agent and its registration
+AssistantAgent informationAgent = await InformationAgent.InformationAgent.GetAgent(settings, client);
+AgentRegistration tradingAgentRegistration = new(informationAgent,
+  "CityWeatherIntent",
+  "You are an assistant that answer questions related to favority cities, weather and city nick names.");
+
+// Create the proxy and add the registred agents
+AgentProxy proxy = new(settings, [salesAgentRegistration, tradingAgentRegistration]);
+
+// Have a conversation with the multi-agent
+await proxy.ProcessForIntent("What is the speed of light?");
+
+await proxy.ProcessForIntent("What was the be product sold?");
+
+await proxy.ProcessForIntent("What is Seattle called?");
+
+// Delete the Assistants, Threads and Files used by the agents
+await salesAgent.DeleteAsync();
+await informationAgent.DeleteAsync();
---
+++ src/cs-sales-copilot/README.md
@@ -0,0 +1,22 @@
+# Two Agent Copilot<br/>with Agent Proxy
+
+This is a sample of two Assistants being used as agents in a complex Copilot solution.
+
+## Diagram
+
+![Two agent Assistant Diagram](images/proxy-two-agent-diagram.png)
+
+## Agents
+
+- **Sales Agent:** It is implemented with an Assistants API that can perform calculations and can provide the user information customer, sellers, customer orders, and inventory.
+- **Information Agent:** It is implemented with an Assistant API with function calling to get mock city and weather information.
+
+## Orchestration
+
+- **Agent Proxy:** This agent coordinates which agent to call depending on user's intent.
+
+## Possible messages handling options
+
+1. No handling: a la LLM model
+2. Manual handling: user write logic to coordinate messages between two agent
+3. Hybrid: One agent keeps state the other just used as LLM
